bdd0797bf07f70e2066fd2a4bdbde60b7a460f72
Sergei Voronkov <sergei.voronkov@stosolutions.com>
2022-06-10 15:21:46 +0200

    Add project files.

---
+++ HiCsvHelper/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace HiCsvHelper
+{
+    using CsvHelper;
+    using CsvHelper.Configuration;
+    using System;
+    using System.Globalization;
+    using System.IO;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            StringWriter writer = new();
+
+            CsvWriter csvWriter = new(
+                writer,
+                new CsvConfiguration(
+                    CultureInfo.CurrentCulture));
+
+            csvWriter.WriteHeader<ReportRecord>();
+
+            csvWriter.NextRecord();
+
+            csvWriter.WriteRecord(
+                new ReportRecord
+                {
+                    Manager = "Ivan Petrov",
+                    Age = 34
+                });
+
+            csvWriter.NextRecord();
+
+            csvWriter.WriteRecord(
+                new ReportRecord
+                {
+                    Manager = "Petr Ivanov",
+                    Age = 43
+                });
+
+
+            csvWriter.Flush();
+
+            Console.WriteLine(writer.ToString());
+        }
+    }
+}
---
+++ HiCsvHelper/ReportRecord.cs
@@ -0,0 +1,15 @@
+﻿namespace HiCsvHelper
+{
+    using CsvHelper.Configuration.Attributes;
+
+    public record ReportRecord
+    {
+
+        [Name("Manager First Name")]
+        public string Manager { get; init; }
+    
+        [Name("Manager Age")]
+        public int Age { get; init; }
+    }
+
+}
